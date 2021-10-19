aba6c56a47be9c6b7c1e19e008c78135c50c2e01
vicilliar <jckim@up.edu.ph>
2021-10-20 00:05:41 +0800

    making basic keyboard script! very raw still. generating 8 keys.

--- scenes/clickables/keyboard.gd
+++ scenes/clickables/keyboard.gd
@@ -1,16 +1,25 @@
 extends Node2D
-export (PackedScene) var key
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var Key = load("key.tcsn")
+var notes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5']
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Generate 8 keys as children (from C4 to C5)
+	for i in range(8):
+		generate_key(i)
 	pass # Replace with function body.
 
 
+func generate_key(i):
+	var new_key = Key.instance()
+	new_key.curr_key = i
+	
+	# This is what we have to figure out! Positioning how?
+	new_key.position.x = 0
+	new_key.position.y = 0
+		
+	add_child(new_key)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
