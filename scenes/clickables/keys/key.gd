4c75867076c95b27dac497abbab5e8914f87fbc2
vicilliar <jckim@up.edu.ph>
2021-10-19 23:51:26 +0800

    most functionality for key scene done. sounds implemented.

---
+++ scenes/clickables/keys/key.gd
@@ -0,0 +1,14 @@
+extends Node2D
+var notes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5']
+var curr_key = 7
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$key_area/AnimatedSprite.play(notes[curr_key] + "_unpressed")
+
+
+func _on_key_area_key_clicked():
+	print(notes[curr_key] + " Clicked")
+	var key_sound = get_node(notes[curr_key] + "_sound")
+	key_sound.play()
