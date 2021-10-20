efb3b3e1abb6dada824f5f6c03e82df324ee20e8
vicilliar <jckim@up.edu.ph>
2021-10-20 12:17:07 +0800

    fixed key sound link. instantiating keys onto keyboard.

--- scenes/clickables/key.gd
+++ scenes/clickables/key.gd
@@ -1,13 +1,17 @@
 extends Node2D
 var notes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5']
-var curr_key = 7
+var curr_key = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pass
+
+
+func set_curr_key(i):
+	curr_key = i
 	$key_area/AnimatedSprite.play(notes[curr_key] + "_unpressed")
-
-
+	
 func _on_key_area_key_clicked():
 	print(notes[curr_key] + " Clicked")
 	var key_sound = get_node(notes[curr_key] + "_sound")
--- scenes/clickables/keyboard.gd
+++ scenes/clickables/keyboard.gd
@@ -1,25 +1,24 @@
 extends Node2D
-var Key = load("key.tcsn")
+# var Key = load("key.tcsn")
 var notes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5']
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Generate 8 keys as children (from C4 to C5)
-	for i in range(8):
-		generate_key(i)
-	pass # Replace with function body.
+	$C4.set_curr_key(0)
+	$D4.set_curr_key(1)
 
 
-func generate_key(i):
-	var new_key = Key.instance()
-	new_key.curr_key = i
+
+# func generate_key(i):
+	# var new_key = Key.instance()
+	# new_key.curr_key = i
 	
 	# This is what we have to figure out! Positioning how?
-	new_key.position.x = 0
-	new_key.position.y = 0
+	# new_key.position.x = 0
+	# new_key.position.y = 0
 		
-	add_child(new_key)
+	# add_child(new_key)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
