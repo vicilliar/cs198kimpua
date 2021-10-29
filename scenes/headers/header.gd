b8de3c9a5de667b052c1a350346657cfe467556d
aimeejuliapua <71208989+aimeejuliapua@users.noreply.github.com>
2021-10-29 11:23:29 +0800

    Custom Clickable Area for Header

--- scenes/headers/header.gd
+++ scenes/headers/header.gd
@@ -9,6 +9,13 @@ func reskin_header(interval):
 	
 	$header_button.set_normal_texture(normal_texture)
 	$header_button.set_pressed_texture(normal_texture)
+	
+	var bitmap_img = Image.new()
+	bitmap_img.load("res://assets/images/scenes/zen_mode/header_bitmap.png")
+	var bitmap = BitMap.new()
+	bitmap.create_from_image_alpha(bitmap_img)
+	
+	$header_button.set_click_mask(bitmap)
 
 func _on_header_pressed():
 	print("Header Touched")
