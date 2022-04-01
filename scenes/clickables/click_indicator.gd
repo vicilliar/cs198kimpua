5d1da0151437f4be349dab83c504d6f47d1af8b6
aimeejuliapua <71208989+aimeejuliapua@users.noreply.github.com>
2022-04-01 23:00:54 +0800

    Level end sfx

--- scenes/clickables/click_indicator.gd
+++ scenes/clickables/click_indicator.gd
@@ -2,8 +2,6 @@ extends Node2D
 var interval_state
 var to_destroy
 
-# TODO aimee: (add spriteframes for other intervals/actions, follow naming convention. 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	to_destroy = false
 	$AnimatedSprite.set_animation(str(interval_state) + "_default")
--- scenes/home.gd
+++ scenes/home.gd
@@ -3,6 +3,7 @@ extends Node
 func _ready():
 	# always play main menu music when we come back home.
 	get_tree().get_root().get_node("persistent/main_menu_music").play()
+	
 
 func on_zen_mode_pressed():
 	get_tree().get_root().get_node("persistent/sfx_zen_button").play()
@@ -11,5 +12,6 @@ func on_zen_mode_pressed():
 
 
 func _on_button_game_mode_pressed():
+
 	get_tree().get_root().get_node("persistent/sfx_button").play()
 	get_tree().change_scene("res://scenes/level_selection.tscn")
