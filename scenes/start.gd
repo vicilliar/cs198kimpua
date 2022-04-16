30294a470bffd49f78ba08b63afecff7d186d855
aimeejuliapua <71208989+aimeejuliapua@users.noreply.github.com>
2022-04-16 18:35:26 +0800

    track minutes played

--- scenes/start.gd
+++ scenes/start.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
+var secs_played
 var mins_played
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	global.load_time()
+	secs_played = global.total_time_played
+	
+	mins_played = secs_played/60
+	
 	if mins_played and mins_played > 0:
-		$time_played.set_bbcode("[center]" + str(mins_played) + " minutes played[/center]")
+		if mins_played == 1:
+			$time_played.set_bbcode("[center]" + str(mins_played) + " minute played[/center]")
+		else:
+			$time_played.set_bbcode("[center]" + str(mins_played) + " minutes played[/center]")
 
 func _on_background_pressed():
 	get_tree().change_scene("res://scenes/home.tscn")
